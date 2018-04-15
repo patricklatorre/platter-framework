db0bc867c4444b9f4f0dd11b1f69f3bc6d89999c
Patrick Latorre <patrick_latorre@dlsu.edu.ph>
2018-04-16 00:45:15 +0800

    removed NULLPlatter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,6 @@
 package app;
 
-import app.platter.NULLPlatter;
+import app.platter.SamplePlatter;
 import app.service.platter.model.Platter;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Platter application = new NULLPlatter();
+        Platter application = new SamplePlatter();
 
         application.serve();
     }
